8a99b9c700252ff3fb674d4441a3c3a047513042
fanghengwei <fanghengwei@gmail.com>
2019-04-20 22:20:34 +0800

    no message

--- application/api/common.php
+++ application/api/common.php
@@ -1,1 +1,10 @@
 <?php
+
+function json_return($data=[],$code=1,$msg='操作成功'){
+    $return = [
+        'data'=>$data,
+        'code'=>$code,
+        'msg'=>$msg,
+    ];
+    return json($return)->send();
+}
