9b2e2d52092d4300b9b633d8b8672d3d51570546
fanghengwei <fanghengwei@gmail.com>
2019-04-24 22:33:31 +0800

    no message

--- application/api/controller/Company.php
+++ application/api/controller/Company.php
@@ -3,13 +3,13 @@
 namespace app\api\controller;
 
 use think\Controller;
-use app\api\model\Position as PositionModel;
+use app\api\model\Company as CompanyModel;
 
-class Position extends Controller
+class Company extends Controller
 {
-    public function getPositionList(){
-        $PositionModel = new PositionModel();
-        $list = $PositionModel->where(['status'=>1])->order('sort desc,create_time desc')->limit(20)->select();
+    public function getCompanyList(){
+        $CompanyModel = new CompanyModel();
+        $list = $CompanyModel->where(['status'=>1])->order('sort desc,create_time desc')->limit(20)->select();
         json_return($list);
     }
 }
